ee814723fed4957b92ad85ba15618ac2290c1f70
beyzaerkan <beyzaerkan@icloud.com>
2023-02-26 22:18:03 +0300

    files and folders organized

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
 
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import TerminalListPage from './Pages/TerminalList/TerminalList';
 
@@ -22,8 +21,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
